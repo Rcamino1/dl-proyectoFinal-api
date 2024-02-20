7a2035ae73f2e340a37b9c60789ba91832adaf4f
Javoc <javier.ochoa.77@gmail.com>
2024-02-20 00:15:34 -0300

    Feat cart model

---
+++ src/server/models/cart.dao.js
@@ -0,0 +1,29 @@
+import db from '../database.js';
+
+const Cart = {
+  async getAllItemsByUserId(userId) {
+    const query = 'SELECT * FROM Carro WHERE id_usuario = $1';
+    const values = [userId];
+    return db(query, values);
+  },
+
+  async addItem(userId, productId, quantity) {
+    const query = 'INSERT INTO Carro (id_usuario, id_producto, cantidad) VALUES ($1, $2, $3) RETURNING *';
+    const values = [userId, productId, quantity];
+    return db(query, values);
+  },
+
+  async updateItemQuantity(cartItemId, newQuantity) {
+    const query = 'UPDATE Carro SET cantidad = $1 WHERE id_carro = $2 RETURNING *';
+    const values = [newQuantity, cartItemId];
+    return db(query, values);
+  },
+
+  async removeItem(cartItemId) {
+    const query = 'DELETE FROM Carro WHERE id_carro = $1';
+    const values = [cartItemId];
+    return db(query, values);
+  }
+};
+
+export default Cart;
