2f563219ec59a9a310ab59197170ebff378c8053
Javoc <javier.ochoa.77@gmail.com>
2024-02-20 00:35:26 -0300

    Build some changes about file structure and paths

--- src/server/routes/user.route.js
+++ src/server/routes/user.route.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const {
   authenticateToken,
   isAdmin,
-} = require("../middlewares/auth.middlewares");
+} = require("../middlewares/auth.middleware");
 const bcrypt = require("bcryptjs");
 
 const router = express.Router();
