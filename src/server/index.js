640b57947ae8e17c4a8b62aaab4b55c08c2b7f5d
Javoc <javier.ochoa.77@gmail.com>
2024-02-20 00:05:27 -0300

    Fix index.js path again

--- src/server/index.js
+++
@@ -1,5 +0,0 @@
-export { default as auth } from "./routes/auth.route.js";
-export { default as cart } from "./routes/cart.route.js";
-export { default as product } from "./routes/product.route.js";
-export { default as user } from "./routes/user.route.js";
-export { default as errors } from "./routes/errors.route.js";
