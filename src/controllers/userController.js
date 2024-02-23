e7798a00b2d0b250115960ee0b9a5567dac15e9c
Rcamino1 <127453851+Rcamino1@users.noreply.github.com>
2024-02-23 01:11:13 -0300

    Pending fix login route
    Login route not reading the password as a string, thus throwing an error.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,6 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  console.log(req.body);
   try {
     const { email, password } = req.body;
     const result = await userService.authenticateUser(email, password);
