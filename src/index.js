a9ed7bcebae952ae79359ad4b5a3ff287fdaf36f
Rcamino1 <127453851+Rcamino1@users.noreply.github.com>
2024-02-25 15:15:39 -0300

    CORS Installed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
